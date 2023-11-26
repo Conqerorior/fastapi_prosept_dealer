55b914a72206cb710a2762b7bf4e7912a57b1d02
Ivan Konyshkin <konyshkinn2001@gmail.com>
2023-11-26 20:33:09 +0500

    DocsAPI GitHub Pages

--- app/main.py
+++ app/main.py
@@ -5,8 +5,3 @@ from app.auth.routers import user_router
 app = FastAPI(title='FastAPI Prosept Dealer')
 
 app.include_router(user_router)
-
-
-@app.get('/')
-async def main():
-    return 'Hello World'
