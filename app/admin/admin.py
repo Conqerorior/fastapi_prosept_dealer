5e3aa1fbc6deb2a8e594625e886f26a9f005d0eb
Серей Овинцев <ozonti@mail.ru>
2023-11-29 11:20:16 +0300

    Настройка Админки

--- app/admin/admin.py
+++ app/admin/admin.py
@@ -12,6 +12,7 @@ def setup_admin(app, engine):
 
         name = 'Дилер'
         name_plural = 'Дилеры'
+        column_labels = {MarketingDealer.name: 'Имя Дилера'}
         column_list = [MarketingDealer.id, MarketingDealer.name]
 
     class MarketingDealerPriceAdmin(ModelView, model=MarketingDealerPrice):
@@ -19,6 +20,18 @@ def setup_admin(app, engine):
 
         name = 'Продукты Дилера'
         name_plural = 'Продукты Дилеров'
+        column_labels = {
+            MarketingDealerPrice.price: 'Цена',
+            MarketingDealerPrice.product_name: 'Имя Продукта',
+            MarketingDealerPrice.product_url: 'Ссылка на Продукт',
+            MarketingDealerPrice.date: 'Дата',
+        }
+        column_searchable_list = [
+            MarketingDealerPrice.product_name
+        ]
+        column_sortable_list = [
+            MarketingDealerPrice.date
+        ]
         column_list = [
             MarketingDealerPrice.id,
             MarketingDealerPrice.price,
@@ -30,8 +43,31 @@ def setup_admin(app, engine):
     class MarketingProductAdmin(ModelView, model=MarketingProduct):
         """Отображение Модели Просепт."""
 
+        def colum_format(self, value):
+            """Функция регулирования ширины колонки"""
+
+            return self.name[:40] if value else ''
+
         name = 'Просепт'
         name_plural = 'Продукты Просепта'
+        column_searchable_list = [
+            MarketingProduct.name
+        ]
+        column_sortable_list = [
+            MarketingProduct.id
+        ]
+        column_labels = {
+            MarketingProduct.name: 'Имя',
+            MarketingProduct.cost: 'Цена',
+            MarketingProduct.ean_13: 'Код Товара',
+            MarketingProduct.article: 'Артикул',
+            MarketingProduct.min_recommended_price: 'Рекомендованная мин. цена',
+            MarketingProduct.recommended_price: 'Рекомендованная Цена',
+        }
+
+        column_formatters = {
+            'name': colum_format
+        }
         column_list = [
             MarketingProduct.id,
             MarketingProduct.name,
