be758028c7e8ae87f268047b01486c8059225165
Серей Овинцев <ozonti@mail.ru>
2023-11-30 17:21:53 +0300

    Тест Патч запроса

--- app/matching/routers.py
+++ app/matching/routers.py
@@ -1,6 +1,7 @@
 from typing import List
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy import func, text
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 
@@ -17,7 +18,7 @@ matching_router = APIRouter()
 @matching_router.get('/matching_product_dealer', tags=['Матчинг'],
                      response_model=List[MatchingProductDealerModel],
                      summary=('Список наиболее вероятных карточек '
-                              'производителя для каждой карточки диллера'))
+                              'производителя для каждой карточки дилера'))
 async def compare_products(db: AsyncSession = Depends(get_db)):
     """Выводим объекты модели «MatchingProductDealer»"""
 
@@ -50,3 +51,47 @@ async def compare_products(db: AsyncSession = Depends(get_db)):
         ))
 
     return response
+
+
+@matching_router.patch(path='/matching_product_dealer/{product_id}',
+                       tags=['Матчинг'],
+                       response_model=List[MatchingProductDealerModel],
+                       summary='Отложить выбранную карточку')
+async def patch_products(product_id: int, db: AsyncSession = Depends(get_db)):
+    matching_products = await get_all_data(db, MatchingProductDealer)
+
+    for matching_product in matching_products:
+        if product_id in matching_product.product_ids:
+            # Изменяем порядок списка, перемещая выбранный элемент в конец
+            matching_product.product_ids.remove(product_id)
+            matching_product.product_ids.append(product_id)
+
+            # Сохраняем изменения
+            await db.commit()
+
+            # Обновляем порядок продуктов в объекте MatchingProductDealer
+            products = await db.execute(select(MarketingProduct).where(
+                MarketingProduct.id.in_(matching_product.product_ids)))
+            products = products.scalars().all()
+
+            # Сортируем продукты в нужном порядке, как в списке product_ids
+            products_dict = {product.id: product for product in products}
+            sorted_products = [products_dict[id] for id in
+                               matching_product.product_ids if id in products_dict]
+
+            # Преобразуем данные
+            lst_dict_products = [item.to_dict() for item in sorted_products]
+            dealerprice = await get_data_by_id(db, MarketingDealerPrice,
+                                               matching_product.dealerprice_id)
+            dict_dealerprice = dealerprice.to_dict()
+
+            # Возвращаем обновленный объект MatchingProductDealer
+            return [
+                MatchingProductDealerModel(
+                    id=matching_product.id,
+                    dealerprice=dict_dealerprice,
+                    products=lst_dict_products
+                )
+            ]
+
+    raise HTTPException(status_code=404, detail="Продукт не найден")
