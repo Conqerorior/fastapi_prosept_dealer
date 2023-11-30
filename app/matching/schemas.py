d9792604deff53dfcdd0b84a5a88c4b757466b5f
Ivan Konyshkin <konyshkinn2001@gmail.com>
2023-11-30 12:45:16 +0500

    Изменил роутер матчинга, удалил ненужные роутеры

--- app/matching/schemas.py
+++ app/matching/schemas.py
@@ -1,11 +1,14 @@
 from pydantic import BaseModel, Field, conlist
 
-from app.products.schemas import MarketingProductModel
+from app.products.schemas import (MarketingDealerPriceModel,
+                                  MarketingProductModel)
 
 
 class MatchingProductDealerModel(BaseModel):
     id: int = Field(description='ID объекта в БД')
-    dealerprice_id: int = Field(description='ID товара от дилера')
+    dealerprice: MarketingDealerPriceModel = Field(
+        description='Карточка товара дилера'
+    )
     products: conlist(
         MarketingProductModel, min_length=5, max_length=5) = Field(
             description='Список который содержит пять товаров от Просепт'
