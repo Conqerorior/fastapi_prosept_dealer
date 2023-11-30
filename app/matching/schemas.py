921fb4369999a06a550eaaf4b89a129197cb99d8
Ivan Konyshkin <konyshkinn2001@gmail.com>
2023-11-30 08:12:41 +0500

    fix routers

--- app/matching/schemas.py
+++ app/matching/schemas.py
@@ -1,8 +1,12 @@
 from pydantic import BaseModel, Field, conlist
 
+from app.products.schemas import MarketingProductModel
+
 
 class MatchingProductDealerModel(BaseModel):
     id: int = Field(description='ID объекта в БД')
     dealerprice_id: int = Field(description='ID товара от дилера')
-    product_ids: conlist(int, min_length=5, max_length=5) = Field(
-        description='Список который содержит пять ID товаров от Просепт')
+    products: conlist(
+        MarketingProductModel, min_length=5, max_length=5) = Field(
+            description='Список который содержит пять товаров от Просепт'
+    )
