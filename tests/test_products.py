95f9728e859b9e489d0d555d68f0067a98769545
Серей Овинцев <ozonti@mail.ru>
2023-12-07 15:45:19 +0300

    Тестирование Базы Данных Products

--- tests/test_products.py
+++ tests/test_products.py
@@ -3,12 +3,10 @@ from urllib.parse import urlparse
 
 from httpx import AsyncClient
 from sqlalchemy import insert, select
-from sqlalchemy.ext.asyncio import AsyncSession
 
-from tests.conftest import async_session_marker
-from app.products.models import (MarketingDealer,
-                                 MarketingDealerPrice,
+from app.products.models import (MarketingDealer, MarketingDealerPrice,
                                  MarketingProduct, MarketingProductDealerKey)
+from tests.conftest import async_session_marker
 
 
 async def data_to_dict(obj):
@@ -105,92 +103,70 @@ async def test_marketing_product(
         res = await session.execute(query)
         marketing_product = res.scalars().all()
         result = await data_to_dict(marketing_product)
-        print(result)
-        print(result)
-        print(result)
-#
-#         expected_data = [{
-#             'id': 1,
-#             'article': '123-02',
-#             'ean_13': '4610093420560.0',
-#             'cost': 189,
-#             'name': 'Средство для удаления наклеек Duty Universal / 210мл',
-#             'recommended_price': 443,
-#             'category_id': '43.0',
-#             'ozon_name': 'Средство для удаления клейкой ленты, клея, наклеек PROSEPT "Duty Universal", 0.2 л.',
-#             'name_1c': 'Средство для удаления клейкой ленты, клея, наклеек PROSEPT "Duty Universal", 0.2 л.',
-#             'wb_name': 'Средство для удаления клейкой ленты, клея, наклеек PROSEPT "Duty Universal", 0.2 л.',
-#             'ozon_article': '445949007.0',
-#             'wb_article': '189522705.0',
-#             'ym_article': '150033482.0',
-#             'wb_article_td': '008-1'
-#         }]
-#
-#         result_db = await session.execute(
-#             select(MarketingDealerPrice).filter_by(id=1))
-#
-#         expected_types = {
-#             'id': int,
-#             'article': str,
-#             'ean_13': str,
-#             'cost': float,
-#             'name': str,
-#             'recommended_price': float,
-#             'category_id': str,
-#             'ozon_name': str,
-#             'name_1c': str,
-#             'wb_name': str,
-#             'ozon_article': str,
-#             'wb_article': str,
-#             'ym_article': str,
-#             'wb_article_td': str
-#         }
-#
-#         exp_type = await compare_type(result, expected_types)
-#
-#         assert exp_type
-#         assert len(result) == 1, "Ожидается только одна запись"
-#         assert result == expected_data, 'Данные не соответствуют'
-#         assert result_db.scalar() is not None, "Запись не найдена в базе данных"
-#
-#
-# async def test_product_dealer_key():
-#     async with async_session_marker() as session:
-#         # test_data = insert(MarketingProductDealerKey).values(
-#         #     id=1,
-#         #     key='546227',
-#         #     product_id=1,
-#         #     dealer_id=1,
-#         # )
-#         #
-#         # await session.execute(test_data)
-#         # await session.commit()
-#
-#         query = select(MarketingProductDealerKey)
-#         res = await session.execute(query)
-#         marketing_product = res.scalars().all()
-#         result = await data_to_dict(marketing_product)
-#
-#         expected_data = [{
-#             'id': 1,
-#             'key': '546227',
-#             'product_id': 1,
-#             'dealer_id': 1,
-#         }]
-#
-#         result_db = await session.execute(
-#             select(MarketingDealerPrice).filter_by(id=1))
-#
-#         expected_types = {
-#             'id': int,
-#             'key': str,
-#             'product_id': int,
-#             'dealer_id': int,
-#         }
-#
-#         exp_type = await compare_type(result, expected_types)
-#
-#         assert exp_type
-#         assert len(result) == 1, "Ожидается только одна запись"
-#         assert result == expected_data, 'Данные не соответствуют'
-#         assert result_db.scalar() is not None, "Запись не найдена в базе данных"
+        result_db = await session.execute(
+            select(MarketingDealerPrice).filter_by(id=1))
+
+        expected_types = {
+            'id': int,
+            'article': str,
+            'ean_13': str,
+            'cost': float,
+            'name': str,
+            'recommended_price': float,
+            'category_id': str,
+            'ozon_name': str,
+            'name_1c': str,
+            'wb_name': str,
+            'ozon_article': str,
+            'wb_article': str,
+            'ym_article': str,
+            'wb_article_td': str
+        }
+
+        exp_type = await compare_type(result, expected_types)
+
+        assert exp_type, 'Типы данных не соответствуют'
+        assert len(result) == 5, "Ожидается только 5 записей"
+        assert result_db.scalar() is not None, "Запись не найдена в базе данных"
+
+
+async def test_product_dealer_key():
+    async with async_session_marker() as session:
+        test_data = insert(MarketingProductDealerKey).values(
+            id=1,
+            key='1234567890',
+            product_id=1,
+            dealer_id=1,
+        )
+
+        await session.execute(test_data)
+        await session.commit()
+
+        query = select(MarketingProductDealerKey)
+        res = await session.execute(query)
+        marketing_product = res.scalars().all()
+        result = await data_to_dict(marketing_product)
+
+        expected_data = [{
+            'id': 1,
+            'key': '1234567890',
+            'product_id': 1,
+            'dealer_id': 1,
+        }]
+
+        result_db = await session.execute(
+            select(MarketingDealerPrice).filter_by(id=1))
+
+        expected_types = {
+            'id': int,
+            'key': str,
+            'product_id': int,
+            'dealer_id': int,
+        }
+
+        exp_type = await compare_type(result, expected_types)
+
+        assert exp_type
+        assert len(result) == 1, "Ожидается только одна запись"
+        assert result == expected_data, 'Данные не соответствуют'
+        assert result_db.scalar() is not None, "Запись не найдена в базе данных"
